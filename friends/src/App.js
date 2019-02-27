b76975aa51be124b83276b230343e7633a3e1b6e
dislersd <dislersd@gmail.com>
2019-02-27 12:53:08 -0800

    fetched data using axios and sent to a friendslist component

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,25 +1,33 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import FriendsList from "./components/FriendsList";
+import axios from "axios";
+import { Route } from "react-router-dom";
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      friends: [],
+      error: ""
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/friends")
+      .then(res => this.setState({ friends: res.data }))
+      .catch(err => this.setState({ error: err }));
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Route
+          exact
+          path="/"
+          render={props => <FriendsList {...this.state} {...props} />}
+        />
       </div>
     );
   }
